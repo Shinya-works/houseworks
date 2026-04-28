c75db2a4db6d1529f2e7347543b7dd4f9a2ed757
shinya_works <shinya.works.w@gmail.com>
2026-04-28 23:39:28 +0900

    stateの定義

--- apps/manager/store/task.ts
+++ apps/manager/store/task.ts
@@ -1,32 +1,30 @@
 import { defineStore, useNuxtApp } from '#imports'
-import { ref, onMounted } from "vue";
+import snakecaseKeys from 'snakecase-keys'
+import camelcaseKeys from 'camelcase-keys'
 
-export const useTaskStore = defineStore("tasks", () => {
+export const useTaskStore = defineStore("tasks", {
   // 取得した値を格納する変数（今回はIPアドレスが文字列で入る）
-  const tasks = ref<string | null>(null);
-
-  // APIを呼び出す関数
-  const fetchTasks = async () => {
-    const { $axios } = useNuxtApp()
-    try {
-      // icanhazip.com はプレーンテキストを返すため、response.data に文字列が入ります
-      const response = await $axios.get("v1/tasks");
-    //   // 取得した値を tasks に代入（改行コードが含まれる場合があるため trim() しています）
-      tasks.value = response.data;
-    } catch (error) {
-      console.error("データ取得に失敗しました:", error);
-      tasks.value = "Error fetching data";
+  state: () => {
+    return{
+      task: { id: '' },
+      tasks: [
+        {
+          id: '',
+          title: '',
+          created_at: '',
+        },
+      ]
     }
-  };
+  },
 
-  // ストアが初期化された時に実行したい場合は onMounted などを使用
-  // またはコンポーネント側から明示的に呼び出す形でもOKです
-  onMounted(() => {
-    fetchTasks();
-  });
-
-  return { 
-    tasks,
-    fetchTasks // 関数も返しておくと再取得が可能になります
-  };
-});
+  actions: {
+    async fetchTasks() {
+      const { $axios } = useNuxtApp();
+      const response = await $axios.get(
+        "v1/tasks",
+      );
+      const data = camelcaseKeys(response.data?.data, { deep: true })
+      this.tasks = data
+    }
+  }
+}); 
